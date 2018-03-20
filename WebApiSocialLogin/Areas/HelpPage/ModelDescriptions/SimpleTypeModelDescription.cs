7237989619ed217fbcc022c5b7d9f2b12e0e286e
suryaprakashgupta <suryaprakash.gupta1@gmail.com>
2018-03-21 00:56:24 +0530

    first commit

---
+++ WebApiSocialLogin/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApiSocialLogin.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ WebApiSocialLogin/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web.Http;
+using WebApiSocialLogin.Models;
+
+namespace WebApiSocialLogin.Controllers
+{
+    public class AccountController : ApiController
+    {
+        [Route("api/User/Register")]
+        [HttpPost]
+        [AllowAnonymous]
+        public IdentityResult Register(AccountModel model)
+        {
+            var userStore = new UserStore<ApplicationUser>(new ApplicationDbContext());
+            var manager = new UserManager<ApplicationUser>(userStore);
+            var user = new ApplicationUser() { UserName = model.UserName, Email = model.Email };
+            user.FirstName = model.FirstName;
+            user.LastName = model.LastName;
+            
+            IdentityResult result = manager.Create(user, model.Password);
+            return result;
+        }
+
+        [HttpGet]
+        [Route("api/GetUserClaims")]
+        
+        public AccountModel GetUserClaims()
+        {
+            var identityClaims = (ClaimsIdentity)User.Identity;
+            IEnumerable<Claim> claims = identityClaims.Claims;
+            AccountModel model = new AccountModel()
+            {
+                UserName = identityClaims.FindFirst("Username").Value,
+                Email = identityClaims.FindFirst("Email").Value,
+                FirstName = identityClaims.FindFirst("FirstName").Value,
+                LastName = identityClaims.FindFirst("LastName").Value,
+                LoggedOn = identityClaims.FindFirst("LoggedOn").Value
+            };
+            return model;
+        }
+    }
+}
